fe7a493ad0702bb023554516ab0280ad3b7a7cc8
ramyakrishnan86 <ramya_krishnan86@yahoo.co.in>
2018-10-16 12:05:59 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,7 +1,8 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
@@ -9,4 +10,17 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+def percentile_k_features(df,k = 20):
+    X = df.drop(['SalePrice'], axis = 1)
+    y = df['SalePrice']
+
+    selector = SelectPercentile(f_regression, k)
+    X_new = selector.fit_transform(X, y)
+ 
+    featurelist = list(X.columns.values[np.argsort(selector.scores_)[-1:-X_new.shape[1]-1:-1]])
+    
+    return featurelist
+
+# percentile_k_features(data,20)
+
 
